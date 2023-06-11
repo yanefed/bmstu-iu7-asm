381190b4d99c1c09f58a9f57351fdff164531c7c
Ivan Bogatyrev <yanagiori@gmail.com>
2023-06-11 14:29:13 +0300

    wiki?

--- 7/inc/my_strlen.h
+++
@@ -1,8 +0,0 @@
-#ifndef MY_STRLEN_H
-#define MY_STRLEN_H
-
-#include <stdlib.h>
-
-size_t my_strlen(const char *const string);
-
-#endif //MY_STRLEN_H
--- README.md
+++
@@ -1,4 +0,0 @@
-# bmstu-iu7-asm
-На память.
-
-[Подготовка к экзамену](https://github.com/yanagi-ori/bmstu-iu7-asm/wiki)
